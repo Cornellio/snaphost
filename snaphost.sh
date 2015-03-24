7e62706f08f63540f71fd2feef8bb8500e55b471
cornellio <peter.cornell@me.com>
2015-03-24 16:13:51 -0700

    dry up

--- snaphost.sh
+++ snaphost.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 #
-# A bare bones shell script to aide in testing a Puppet manifest for Riak
-# Can revert or validate a Riak install on RHEL ditros
+# A bare bones shell script to aide in testing Puppet manifests
+# Can revert the install of packages and files or validate a good install
 # Assumes sudo requiretty disabled on remote hosts and sudo as root enabled
 #
-# https://github.com/Cornellio/puppet-riak
+# https://github.com/Cornellio/snaphost
 
 # TODO
-# dry-ify
 # add check functions
 
-HOSTLIST="ppriakops01-sc9 ppriakops02-sc9 ppriakops03-sc9"
-# HOSTLIST="ppriakops01-sc9"
 SSH_REMOTE="/usr/bin/ssh -q -t"
+
 
 parse_args () {
 
-  while getopts "d:p:" OPTION; do
+  if [ $# -lt 2 ] ; then
+    usage
+  fi
 
+  while getopts "d:p:h:" OPTION; do
     case ${OPTION} in
       d)
         dirs_to_remove="$OPTARG"
@@ -25,15 +26,18 @@ parse_args () {
       p)
         pkgs_to_remove="$OPTARG"
         ;;
-      ?) usage
+      h)
+        hosts="$OPTARG"
+        hosts=${hosts/,/ }
+        hosts_num=$( echo ${hosts} | wc -w )
+        hosts_num=${hosts_num// /}
+        ;;
+      *)
+        usage
         ;;
     esac
-
   done
-
   shift
-  # shift $(($OPTIND-1))
-
 }
 
 
@@ -44,96 +48,62 @@ usage () {
   $PROGNAME usage:
 
   Removal commands:
-  $PROGNAME remove -d [directories to remove] -p [packages to remove]
-  Remove given packages and directores on each host
+  $PROGNAME remove -d [directories to remove,] -p [packages to remove,]
+  Remove given packages and directories on each host. Use a comma separated list.
 
   Check commands:
   $PROGNAME check
   Check status of services on each host
 
+  Examples:
+
+  Remove Riak
+  snaphost.sh -p riak,erlang-rebar -d /etc/riak,/var/lib/riak -h ppriakops01-sc9
+
 EOF
+exit
 }
 
 
-worker () {
+remove_objects () {
 
-  case $1 in
+  object_type="$1"
+  shift
+  OBJECTS=$*
+  OBJECTS_NUM="$#"
 
-    remove)
-      for host in $HOSTLIST; do
-        printf "\nREMOVING\nHOST: $host\n"
-        ssh -q -t $host "sudo yum -y remove riak erlang-rebar ; sudo rm -fr /etc/riak /var/lib/riak"
-      done
+  case $object_type in
+    packages)
+      object_description="packages"
+      command="sudo yum -y remove"
       ;;
-
-    check)
-      for host in $HOSTLIST; do
-        printf "\nCHECKING\nHOST: $host\n"
-        ssh -q -t $host "/sbin/service riak status; ls /var/lib/riak"
-        ssh -q -t $host "sudo /usr/sbin/riak-admin test"
-        ssh -q -t $host "sudo /usr/sbin/riak-admin status | grep ring_creation_size"
-        ssh -q -t $host "sudo /usr/sbin/riak-admin status | grep ring_ownership"
-      done
-      ;;
-
-    *)
-      echo "Give something I can use. Try again"
-      ;;
-
+    directories)
+      object_description="directories"
+      command="sudo rm -vfr"
   esac
 
-}
+  printf "\n${OBJECTS_NUM} ${object_description} marked for removal: ${OBJECTS}\n"
 
-
-remove_pkgs () {
-
-  pkgs_to_remove=$*
-  pkgs_to_remove_num="$#"
-
-  printf "\nPackages marked for removal: ${pkgs_to_remove}\n"
-
-  for host in $HOSTLIST; do
-    printf "\nHOST ${host}:\n\n"
-    for pkg in $pkgs_to_remove; do
-      $SSH_REMOTE $host "sudo yum -y remove ${pkg}"
+  for host in $hosts; do
+    printf "\nAccessing host: ${host}\n\n"
+    for object in $OBJECTS; do
+      $SSH_REMOTE $host "${command} ${object}"
     done
   done
 
 }
-
-remove_dirs () {
-
-  dirs_to_remove=$*
-  dirs_to_remove_num="$#"
-
-  printf "\nDirectories marked for removal: ${dirs_to_remove}\n"
-
-  for host in $HOSTLIST; do
-    printf "\nHOST ${host}:\n\n"
-    for dir in $dirs_to_remove; do
-      $SSH_REMOTE $host "sudo rm -vfr ${dir}"
-    done
-  done
-
-}
-
-
 
 
 parse_args $*
 
-# echo ${dirs_to_remove}
+printf "\nStarting removal process on ${hosts_num} hosts.\n"
 
-# Remove packages
 if [ $pkgs_to_remove ]; then
-  printf "\nStarting removal process for packages.\n"
   pkgs_to_remove=${pkgs_to_remove/,/ }
-  remove_pkgs ${pkgs_to_remove}
+  remove_objects packages $pkgs_to_remove
 fi
 
-# Remove files
 if [ $dirs_to_remove ]; then
-  printf "\nStarting removal process for directores.\n"
   dirs_to_remove=${dirs_to_remove/,/ }
-  remove_dirs ${dirs_to_remove}
+  remove_objects directories $dirs_to_remove
 fi
